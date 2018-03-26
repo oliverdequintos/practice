9f37b1d1d5a44315fbd5b72d8be76fbee665ac62
Oliver <oliverdequintos@yahoo.com>
2018-03-26 17:58:27 +0800

    Added getter and setter method to save full_name in first_name and last_name attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  # Getter
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+
+  # Setter (get the values from input field and assign to particular fields)
+  def full_name=(full_name_field)
+    split = full_name_field.split(' ', 2)
+    self.first_name = split.first
+    self.last_name = split.last
+  end
 end
